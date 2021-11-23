9a81f7c2d84213c91e3935013d099f358e143db9
Toutsu <hegin4@gmail.com>
2021-11-23 11:31:48 +1000

    Adding SwaggerConfig and test Controller

---
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TestController.java
@@ -0,0 +1,18 @@
+package com.javamentor.qa.platform.webapp.controllers.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public List<String> getTest(){
+        return new ArrayList<>();
+    }
+}
