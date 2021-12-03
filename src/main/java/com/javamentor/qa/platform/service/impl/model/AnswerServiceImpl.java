52631d8a1eecf0b71b4559cee8b22f3719f07a89
Ilya Anikaev <anikaevilya@gmail.com>
2021-12-03 14:16:33 +0300

    1.0

---
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/AnswerServiceImpl.java
@@ -0,0 +1,18 @@
+package com.javamentor.qa.platform.service.impl.model;
+
+import com.javamentor.qa.platform.dao.abstracts.model.AnswerDao;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
+import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AnswerServiceImpl extends ReadWriteServiceImpl<Answer, Long> implements AnswerService {
+
+    @Autowired
+    public AnswerServiceImpl(AnswerDao answerDao) {
+        super(answerDao);
+    }
+}
