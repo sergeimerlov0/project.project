0bf7ae2f349defd9b8d935241b96ae2d5d6dad37
Toutsu <hegin4@gmail.com>
2021-12-04 23:14:00 +1000

    Переделал без использования JPA

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -1,5 +1,7 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.dao.abstracts.model.AnswerDao;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/user/question/{questionId}/answer")
 @Api(value = "Работа с ответами на вопросы", tags = {"Ответ на вопрос"})
 public class ResourceAnswerController {
+    private final AnswerDao answerDao;
 
     @ApiOperation(value = "Удаление ответа на вопрос", tags = {"Удаление ответа"})
     @ApiResponses(value =
                 @ApiResponse(code = 200, message = "Успешное удаление"))
     @DeleteMapping("/{answerId}")
-    public void deleteAnswerById(@PathVariable Long answerId, @PathVariable Long questionId) {
-
+    public void deleteAnswerById(@ApiParam("Id ответа") @PathVariable Long answerId,
+                                 @ApiParam("Id вопроса") @PathVariable Long questionId) {
+        answerDao.deleteById(answerId);
     }
 }
