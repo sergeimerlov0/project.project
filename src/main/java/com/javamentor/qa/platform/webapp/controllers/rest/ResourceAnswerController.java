1a620ea7c26666bf042ad5ce79a445f83393f62e
Toutsu <hegin4@gmail.com>
2021-12-07 01:07:43 +1000

    Переделал без использования ID вопроса

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -1,24 +1,25 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
-import com.javamentor.qa.platform.dao.abstracts.model.AnswerDao;
-import com.javamentor.qa.platform.models.entity.question.answer.Answer;
+import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/user/question/{questionId}/answer")
 @Api(value = "Работа с ответами на вопросы", tags = {"Ответ на вопрос"})
 public class ResourceAnswerController {
-    private final AnswerDao answerDao;
+    private final AnswerService answerService;
 
     @ApiOperation(value = "Удаление ответа на вопрос", tags = {"Удаление ответа"})
     @ApiResponses(value =
-                @ApiResponse(code = 200, message = "Успешное удаление"))
+    @ApiResponse(code = 200, message = "Успешное удаление"))
     @DeleteMapping("/{answerId}")
-    public void deleteAnswerById(@ApiParam("Id ответа") @PathVariable Long answerId,
-                                 @ApiParam("Id вопроса") @PathVariable Long questionId) {
-        answerDao.deleteById(answerId);
+    public void deleteAnswerById(@ApiParam("Id ответа") @PathVariable Long answerId) {
+        answerService.deleteById(answerId);
     }
 }
