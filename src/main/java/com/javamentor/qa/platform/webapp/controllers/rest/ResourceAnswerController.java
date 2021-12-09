c7784aabe133d21b033c3a8cca5e76513fd563fa
Toutsu <hegin4@gmail.com>
2021-12-10 01:03:02 +1000

    переправил в соответствии с требованиями

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -1,12 +1,17 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +24,11 @@ public class ResourceAnswerController {
     @ApiResponses(value =
     @ApiResponse(code = 200, message = "Успешное удаление"))
     @DeleteMapping("/{answerId}")
-    public void deleteAnswerById(@ApiParam("Id ответа") @PathVariable Long answerId) {
+    public ResponseEntity<String> deleteAnswerById(@ApiParam("Id ответа") @PathVariable Long answerId){
+        if (!answerService.existsById(answerId)) {
+            return ResponseEntity.badRequest().body("Answer with this ID was not found");
+        }
         answerService.deleteById(answerId);
+        return ResponseEntity.status(HttpStatus.OK).body("Answer successfully deleted");
     }
 }
--- src/test/java/com/javamentor/qa/platform/api/ResourceAnswerControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/api/ResourceAnswerControllerTest.java
@@ -2,20 +2,17 @@ package com.javamentor.qa.platform.api;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.javamentor.qa.platform.AbstractApiTest;
-import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ResourceAnswerControllerTest extends AbstractApiTest {
-    @Autowired
-    private AnswerService answerService;
 
     @Test
     @DataSet("datasets/answerDatasets/answerDeleteDataSet.yml")
-    void deleteAnswerById() {
-        answerService.deleteById(100L);
-        assertEquals(2L, answerService.getAll().size());
+    void deleteAnswerById() throws Exception {
+        this.mvc.perform(delete("/api/user/question/100/answer/100"))
+                .andExpect(status().isOk());
     }
 }
