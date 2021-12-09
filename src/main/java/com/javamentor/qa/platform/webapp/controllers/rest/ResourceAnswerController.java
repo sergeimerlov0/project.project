f826cb6f855b2df22bfb53440929cc97f938ccf0
Toutsu <hegin4@gmail.com>
2021-12-10 01:04:13 +1000

    переправил в соответствии с требованиями

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -1,6 +1,5 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
-import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +21,7 @@ public class ResourceAnswerController {
     @ApiResponses(value =
     @ApiResponse(code = 200, message = "Успешное удаление"))
     @DeleteMapping("/{answerId}")
-    public ResponseEntity<String> deleteAnswerById(@ApiParam("Id ответа") @PathVariable Long answerId){
+    public ResponseEntity<String> deleteAnswerById(@ApiParam("Id ответа") @PathVariable Long answerId) {
         if (!answerService.existsById(answerId)) {
             return ResponseEntity.badRequest().body("Answer with this ID was not found");
         }
