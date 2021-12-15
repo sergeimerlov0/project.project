89a8fef9cefd1dced6e22436ab3f0e30d84d1b02
stedbbc <swpbbc@gmail.com>
2021-12-16 00:37:20 +0300

    add test

---
+++ src/test/java/com/javamentor/qa/platform/api/TestUserResourseController.java
@@ -0,0 +1,59 @@
+package com.javamentor.qa.platform.api;
+
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.junit5.api.DBRider;
+import com.javamentor.qa.platform.webapp.configs.JmApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DBRider
+@SpringBootTest(classes = JmApplication.class)
+@TestPropertySource(properties = "spring.config.location = src/test/resources/application-test.properties")
+@AutoConfigureMockMvc
+@DBUnit(caseSensitiveTableNames = true, cacheConnection = false, allowEmptyFields = true)
+public class TestUserResourseController {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DataSet(value = {"dataset/UserResourceController/users.yml",
+            "dataset/UserResourceController/answers.yml",
+            "dataset/UserResourceController/questions.yml",
+            "dataset/UserResourceController/reputations.yml",
+            "dataset/UserResourceController/roles.yml"})
+    void getUserById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/101"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(101))
+                .andExpect(jsonPath("$.email").value("SomeEmail@mail.mail"))
+                .andExpect(jsonPath("$.fullName").value("Max"))
+                .andExpect(jsonPath("$.linkImage").value("link"))
+                .andExpect(jsonPath("$.city").value("Moscow"))
+                .andExpect(jsonPath("$.reputation").value(101));
+    }
+
+    @Test
+    @DataSet(value = {"dataset/UserResourceController/users.yml",
+            "dataset/UserResourceController/answers.yml",
+            "dataset/UserResourceController/questions.yml",
+            "dataset/UserResourceController/reputations.yml",
+            "dataset/UserResourceController/roles.yml"})
+    void shouldNotGetUserById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/105"))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.id").doesNotExist());
+    }
+}
