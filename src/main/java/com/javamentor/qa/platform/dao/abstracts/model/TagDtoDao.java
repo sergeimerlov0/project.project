4cb74bb416e377bb6935896770b1d3f01db7a44f
Toutsu <hegin4@gmail.com>
2021-12-17 15:21:58 +1000

    Реализовал добавление списка TagDto в QuestionDto

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/TagDtoDao.java
+++
@@ -1,9 +0,0 @@
-package com.javamentor.qa.platform.dao.abstracts.model;
-
-public interface TagDtoDao {
-    Long getId();
-
-    String getName();
-
-    String getDescription();
-}
--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/QuestionDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/QuestionDtoDaoImpl.java
@@ -2,15 +2,11 @@ package com.javamentor.qa.platform.dao.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.QuestionDtoDao;
 import com.javamentor.qa.platform.models.dto.QuestionDto;
-import com.javamentor.qa.platform.models.dto.TagDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,11 +17,6 @@ public class QuestionDtoDaoImpl implements QuestionDtoDao {
 
     @Override
     public Optional<QuestionDto> getQuestionDtoByQuestionId(Long id) {
-        List<TagDto> tagDtoList1 = new ArrayList<>();
-        Long authorRepo = 0L;
-
-        Query tagDtoList = entityManager.createQuery("SELECT new com.javamentor.qa.platform.models.dto." +
-                "TagDto(tag.id, tag.name, tag.description) from Tag tag join tag.questions as question where question.id=: id");
 
         return entityManager.createQuery("SELECT new com.javamentor.qa.platform.models.dto." +
                 "QuestionDto(question.id, question.title, author.id, 0L, author.fullName, author.imageLink, " +
@@ -33,7 +24,7 @@ public class QuestionDtoDaoImpl implements QuestionDtoDao {
                 "from Question question " +
                 "join question.user as author " +
                 "join question.answers as answer " +
-                "where question.id=: id", QuestionDto.class).setParameter("id", id).getResultStream().findAny();
+                "where question.id = :id", QuestionDto.class).setParameter("id", id).getResultStream().findAny();
     }
 
     //JPa конструктор, result transformer.
