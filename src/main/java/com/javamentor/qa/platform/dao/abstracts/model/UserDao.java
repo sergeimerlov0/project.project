46fe383b324b5e6c20888de2e7773bebdfa23a80
Ilya Anikaev <anikaevilya@gmail.com>
2021-12-20 15:38:36 +0300

    2.1

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/UserDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/model/UserDao.java
@@ -2,6 +2,6 @@ package com.javamentor.qa.platform.dao.abstracts.model;
 
 import com.javamentor.qa.platform.models.entity.user.User;
 
-public interface UserDao extends ReadWriteDao<User, Long>  {
+public interface UserDao extends ReadWriteDao<User, Long> {
     User getByUsername(String username);
 }
