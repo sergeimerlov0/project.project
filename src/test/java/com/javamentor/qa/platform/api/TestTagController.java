a4976a497c144ed342de2633fca74f8e9be4e4ad
Ilya Anikaev <anikaevilya@gmail.com>
2021-12-22 15:17:37 +0300

    2.3

--- src/test/java/com/javamentor/qa/platform/api/TestTagController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestTagController.java
@@ -2,7 +2,6 @@ package com.javamentor.qa.platform.api;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.javamentor.qa.platform.AbstractApiTest;
-import com.javamentor.qa.platform.models.entity.question.Tag;
 import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,7 +32,10 @@ class TestTagController extends AbstractApiTest {
     void getAllIgnoredTagDto() throws Exception {
         this.mvc.perform(get("/api/user/tag/ignored"))
                 .andExpect(status().isOk());
-        Assertions.assertTrue(idTest(1));
+        Assertions.assertTrue(entityManager.createQuery("select t.ignoredTag.id from IgnoredTag t where t.user.id=:id", Long.class)
+                .setParameter("id", 2L)
+                .getResultList()
+                .containsAll(Arrays.asList(100L, 101L)));
     }
 
     @Test
@@ -42,27 +45,9 @@ class TestTagController extends AbstractApiTest {
     void getAllTrackedTagDto() throws Exception {
         this.mvc.perform(get("/api/user/tag/tracked"))
                 .andExpect(status().isOk());
-        Assertions.assertTrue(idTest(0));
-    }
-
-    boolean idTest(int x) {
-        if (x == 1) {
-            for (Long id : entityManager.createQuery("select t.ignoredTag.id from IgnoredTag t where t.user.id=:id", Long.class)
-                    .setParameter("id", 2L)
-                    .getResultList()) {
-                if (!(id == 100 || id == 101)) {
-                    return false;
-                }
-            }
-        } else {
-            for (Long id : entityManager.createQuery("select t.trackedTag.id from TrackedTag t where t.user.id=:id", Long.class)
-                    .setParameter("id", 2L)
-                    .getResultList()) {
-                if (!(id == 102 || id == 103)) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        Assertions.assertTrue(entityManager.createQuery("select t.trackedTag.id from TrackedTag t where t.user.id=:id", Long.class)
+                .setParameter("id", 2L)
+                .getResultList()
+                .containsAll(Arrays.asList(102L, 103L)));
     }
 }
