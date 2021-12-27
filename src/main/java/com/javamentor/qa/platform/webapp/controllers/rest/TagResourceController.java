100c81e23fe3a0d67543df119a96c4d5851029d7
igorzobov <igor_zobov@mail.ru>
2021-12-27 14:02:00 +0700

    Добавлено api добавления ignoredTag и TrackedTag

---
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TagResourceController.java
@@ -0,0 +1,67 @@
+package com.javamentor.qa.platform.webapp.controllers.rest;
+
+import com.javamentor.qa.platform.models.dto.TagDto;
+import com.javamentor.qa.platform.models.entity.question.IgnoredTag;
+import com.javamentor.qa.platform.models.entity.question.Tag;
+import com.javamentor.qa.platform.models.entity.question.TrackedTag;
+import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.service.abstracts.dto.TagDtoService;
+import com.javamentor.qa.platform.service.abstracts.model.IgnoredTagService;
+import com.javamentor.qa.platform.service.abstracts.model.TagService;
+import com.javamentor.qa.platform.service.abstracts.model.TrackedTagService;
+import com.javamentor.qa.platform.service.abstracts.model.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user/tag")
+public class TagResourceController {
+
+    private final TrackedTagService trackedTagService;
+    private final IgnoredTagService ignoredTagService;
+    private final TagDtoService tagDtoService;
+    private final UserService userService;
+    private final TagService tagService;
+
+    @ApiOperation(value = "Добавление тега в TrackedTag", tags = {"TrackedTag"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Тег успешно добавлен в TrackedTag"),
+            @ApiResponse(code = 400, message = "Тега не существует")})
+    @PostMapping("/{id}/tracked")
+    public ResponseEntity<List<TagDto>> addTrackedTag(Authentication authentication, @PathVariable Long id) {
+        Long userId = 2L; // todo убрать когда будет готово секьюрити
+        TrackedTag trackedTag = new TrackedTag();
+        User user = userService.getById(userId).get();
+        Tag tag = tagService.getById(id).get();
+        trackedTag.setTrackedTag(tag);
+        trackedTag.setUser(user);
+        trackedTagService.persist(trackedTag);
+        return new ResponseEntity<>(tagDtoService
+                .getTrackedTagById(userService.getById(userId).get().getId()), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Добавление тега в IgnoredTag", tags = {"IgnoredTag"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Тег успешно добавлен в IgnoredTag"),
+            @ApiResponse(code = 400, message = "Тега не существует")})
+    @PostMapping("/{id}/ignored")
+    public ResponseEntity<List<TagDto>> addIgnoredTag(Authentication authentication, @PathVariable Long id) {
+        Long userId = 2L; // todo убрать когда будет готово секьюрити
+        IgnoredTag ignoredTag = new IgnoredTag();
+        User user = userService.getById(userId).get();
+        Tag tag = tagService.getById(id).get();
+        ignoredTag.setIgnoredTag(tag);
+        ignoredTag.setUser(user);
+        ignoredTagService.persist(ignoredTag);
+        return new ResponseEntity<>(tagDtoService
+                .getIgnoreTagById(userService.getById(userId).get().getId()), HttpStatus.OK);
+    }
+}
