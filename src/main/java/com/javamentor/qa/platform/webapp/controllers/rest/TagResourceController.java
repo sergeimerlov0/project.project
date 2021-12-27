299f7767a91dc0360730f2ce6b02ba2d15a00763
igorzobov <igor_zobov@mail.ru>
2021-12-27 21:55:02 +0700

    разрешены конфликты с dev

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TagResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TagResourceController.java
@@ -9,7 +9,6 @@ import com.javamentor.qa.platform.models.entity.question.IgnoredTag;
 import com.javamentor.qa.platform.models.entity.question.Tag;
 import com.javamentor.qa.platform.models.entity.question.TrackedTag;
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.dto.TagDtoService;
 import com.javamentor.qa.platform.service.abstracts.model.IgnoredTagService;
 import com.javamentor.qa.platform.service.abstracts.model.TagService;
 import com.javamentor.qa.platform.service.abstracts.model.TrackedTagService;
@@ -23,10 +22,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user/tag")
+@Api(value = "Работа с тэгами на вопросы", tags = {"Тэг и вопросы"})
 public class TagResourceController {
 
     private final TrackedTagService trackedTagService;
@@ -34,6 +38,15 @@ public class TagResourceController {
     private final TagDtoService tagDtoService;
     private final UserService userService;
     private final TagService tagService;
+
+    @ApiOperation(value = "Получение списка из 10 тэгов с " +
+            "наибольшим количеством вопросов с данным тэгом", tags = {"Получение списка тэгов"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешное получение")})
+    @GetMapping("/related")
+    public ResponseEntity<List<RelatedTagsDto>> getRelatedTagDto() {
+        return new ResponseEntity<>(tagDtoService.getRelatedTagsDto(), HttpStatus.OK);
+    }
 
     @ApiOperation(value = "Добавление тега в TrackedTag", tags = {"TrackedTag"})
     @ApiResponses(value = {
@@ -67,31 +80,7 @@ public class TagResourceController {
         ignoredTagService.persist(ignoredTag);
         return new ResponseEntity<>(tagDtoService
                 .getIgnoreTagById(userService.getById(userId).get().getId()), HttpStatus.OK);
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-/**
- * \* Created with IntelliJ IDEA.
- * \* User: Rustam
- */
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("api/user/tag")
-@Api(value = "Работа с тэгами на вопросы", tags = {"Тэг и вопросы"})
-public class TagResourceController {
-
-    private final TagDtoService tagDtoService;
-
-    @ApiOperation(value = "Получение списка из 10 тэгов с " +
-            "наибольшим количеством вопросов с данным тэгом", tags = {"Получение списка тэгов"})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Успешное получение")})
-    @GetMapping("/related")
-    public ResponseEntity<List<RelatedTagsDto>> getRelatedTagDto() {
-        return new ResponseEntity<>(tagDtoService.getRelatedTagsDto(), HttpStatus.OK);
     }
 }
