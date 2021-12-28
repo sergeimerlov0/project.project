915a3a88ebb696ee2471ece551accc91fa978a00
Nikita <kotenkov.nikita@mail.ru>
2021-12-28 14:40:24 +0300

    Added swagger documentation

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AuthenticationResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AuthenticationResourceController.java
@@ -6,6 +6,10 @@ import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.security.JwtAuthenticationProvider;
 import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import com.javamentor.qa.platform.webapp.controllers.dto.AuthenticationRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,6 +20,13 @@ import java.util.List;
 import java.util.Objects;
 
 @Slf4j
+@Api(value = "RestController", description = "Controller to authenticate user with JWT")
+@ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Success|OK"),
+        @ApiResponse(code = 403, message = "Forbidden"),
+        @ApiResponse(code = 404, message = "Not found"),
+        @ApiResponse(code = 500, message = "Internal error")
+})
 @RestController
 @RequestMapping("/api")
 public class AuthenticationResourceController {
@@ -28,11 +39,14 @@ public class AuthenticationResourceController {
         this.userService = userService;
     }
 
+    @ApiOperation(value = "Get list of all users ", response = Iterable.class, tags = "users")
     @GetMapping("/getall")
     public List<User> users() throws HttpMessageNotWritableException {
         return userService.getAll();
     }
 
+    @ApiOperation(value = "Generate JWT token and authenticate user",
+            response = String.class, tags = "token")
     @PostMapping("/auth/token")
     public String token(@RequestBody AuthenticationRequest request) throws NullPointerException {
         String email = Objects.requireNonNull(request.getEmail());
