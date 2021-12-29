28da76ebddc762ea40229bcb85dc23660577400c
Valentin <mananathen@gmail.com>
2021-12-29 15:46:05 +0300

    add missing imports

--- src/main/java/com/javamentor/qa/platform/service/impl/model/RoleServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/RoleServiceImpl.java
@@ -1,8 +1,10 @@
 package com.javamentor.qa.platform.service.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.ReadWriteDao;
+import com.javamentor.qa.platform.dao.abstracts.model.RoleDao;
 import com.javamentor.qa.platform.models.entity.user.Role;
 import com.javamentor.qa.platform.service.abstracts.model.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +19,6 @@ public class RoleServiceImpl extends ReadWriteServiceImpl<Role, Long> implements
 
     @Autowired
     private RoleDao roleDao;
-
 
     @Override
     @Transactional
