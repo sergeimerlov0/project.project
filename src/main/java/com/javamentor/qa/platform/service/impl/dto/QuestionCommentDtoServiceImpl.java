00b0d77977903e9eb0e6b1d12ef12ac6d7e2f13f
feromant <maznichenko@list.ru>
2022-01-06 19:59:03 +0300

    add QuestionCommentDtoService and QuestionCommentDtoServiceImpl

---
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/QuestionCommentDtoServiceImpl.java
@@ -0,0 +1,27 @@
+package com.javamentor.qa.platform.service.impl.dto;
+
+import com.javamentor.qa.platform.dao.abstracts.dto.QuestionCommentDtoDao;
+import com.javamentor.qa.platform.models.dto.QuestionCommentDto;
+import com.javamentor.qa.platform.service.abstracts.dto.QuestionCommentDtoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionCommentDtoServiceImpl implements QuestionCommentDtoService {
+
+    private final QuestionCommentDtoDao questionCommentDtoDao;
+
+    public QuestionCommentDtoServiceImpl(QuestionCommentDtoDao questionCommentDtoDao) {
+        this.questionCommentDtoDao = questionCommentDtoDao;
+    }
+
+    @Override
+    @Transactional
+    public List<QuestionCommentDto> getQuestionCommentByQuestionId(Long id) {
+        return questionCommentDtoDao.getQuestionCommentByQuestionId(id);
+    }
+}
