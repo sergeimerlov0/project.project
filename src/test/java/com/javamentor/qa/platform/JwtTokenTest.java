151487d7450b707ba1d45be157d80d3bea2ac56e
Nikita <kotenkov.nikita@mail.ru>
2022-01-07 17:11:39 +0300

    Fixed JwtAuthorizationFilter

--- src/test/java/com/javamentor/qa/platform/JwtTokenTest.java
+++ src/test/java/com/javamentor/qa/platform/JwtTokenTest.java
@@ -1,27 +1,39 @@
 package com.javamentor.qa.platform;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javamentor.qa.platform.webapp.controllers.dto.AuthenticationRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class JwtTokenTest extends AbstractApiTest {
 
     @Test
-    public void JwtProcessing() throws Exception {
-        this.mvc.perform(post("/api/auth/token").contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"email\":\"123@mail.ru\",\"password\":\"123\"}"))
-                .andDo(MockMvcResultHandlers.print())
+    public void checkCorrectWork() throws Exception {
+        String email = "3user@mail.ru";
+        String password = "3111";
+        this.mvc.perform(MockMvcRequestBuilders.get("/api/check/status").header("Authorization", getJwtToken(email, password)))
+                .andDo(print())
                 .andExpect(status().isOk());
     }
 
-    @Test
-    public void forbidden() throws Exception {
-        this.mvc.perform(get("/getall").contentType(MediaType.APPLICATION_JSON))
+    public String getJwtToken(String email, String password) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        AuthenticationRequest request = new AuthenticationRequest();
+        request.setEmail(email);
+        request.setPassword(password);
+        String json = mapper.writeValueAsString(request);
+        MvcResult m = this.mvc.perform(post("/api/auth/token").contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isForbidden());
+                .andExpect(status().isOk())
+                .andReturn();
+        return "Bearer " + m.getResponse().getContentAsString();
     }
 }
