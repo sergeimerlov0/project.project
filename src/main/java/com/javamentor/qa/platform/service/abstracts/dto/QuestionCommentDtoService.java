a30de318efff3ab4e12314120e9351afa4ea168e
feromant <maznichenko@list.ru>
2022-01-08 07:22:03 +0300

    moving QuestionCommentDtoService in QuestionDtoService

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/QuestionCommentDtoService.java
+++
@@ -1,9 +0,0 @@
-package com.javamentor.qa.platform.service.abstracts.dto;
-
-import com.javamentor.qa.platform.models.dto.QuestionCommentDto;
-
-import java.util.List;
-
-public interface QuestionCommentDtoService {
-    List<QuestionCommentDto> getQuestionCommentByQuestionId(Long id);
-}
