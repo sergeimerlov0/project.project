b6e48300cd09ace5846a1508445b58b26d9cdadb
feromant <maznichenko@list.ru>
2022-01-20 09:15:07 +0300

    add CacheConfig and EnableCaching

---
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/CacheConfig.java
@@ -0,0 +1,24 @@
+package com.javamentor.qa.platform.webapp.configs;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+@EnableCaching
+public class CacheConfig extends CachingConfigurerSupport {
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Collections.singletonList(new ConcurrentMapCache("users")));
+        return cacheManager;
+    }
+}
