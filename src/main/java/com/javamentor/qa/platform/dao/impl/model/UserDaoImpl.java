a94029591f8e1262ee7b962dc5f49cb2088c6a64
feromant <maznichenko@list.ru>
2022-01-20 09:20:54 +0300

    Cacheable in method userDAO getByEmail

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/UserDaoImpl.java
@@ -3,6 +3,8 @@ package com.javamentor.qa.platform.dao.impl.model;
 import com.javamentor.qa.platform.dao.abstracts.model.UserDao;
 import com.javamentor.qa.platform.dao.util.SingleResultUtil;
 import com.javamentor.qa.platform.models.entity.user.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -11,6 +13,7 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Repository
 public class UserDaoImpl extends ReadWriteDaoImpl<User, Long> implements UserDao {
 
@@ -18,7 +21,9 @@ public class UserDaoImpl extends ReadWriteDaoImpl<User, Long> implements UserDao
     private EntityManager entityManager;
 
     @Override
+    @Cacheable("users")
     public Optional<User> getByEmail(String email) {
+        log.info("Запрос пользователя из БД в методе getByEmails");
         String hql = "SELECT u FROM User u inner join fetch u.role where u.email = :email";
         TypedQuery<User> query = entityManager.createQuery(hql, User.class).setParameter("email", email);
         return SingleResultUtil.getSingleResultOrNull(query);
