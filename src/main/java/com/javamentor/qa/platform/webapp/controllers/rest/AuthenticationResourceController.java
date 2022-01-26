6c8155dcc4f6da222185484ad5453a262fe0f2e5
feromant <maznichenko@list.ru>
2022-01-26 09:26:15 +0300

    delete method getall(users()) from AuthenticationRequestController

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AuthenticationResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AuthenticationResourceController.java
@@ -11,12 +11,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import java.util.List;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -57,12 +55,6 @@ public class AuthenticationResourceController {
         return user.getAuthorities().contains(roleService.getRoleByName("USER").orElseThrow())
                 ? ResponseEntity.status(HttpStatus.OK).body("Authorization successful")
                 : ResponseEntity.status(HttpStatus.FORBIDDEN).body("Authorization failed");
-    }
-
-    @ApiOperation(value = "Get list of all users ", response = Iterable.class, tags = "users")
-    @GetMapping("/getall")
-    public List<User> users() throws HttpMessageNotWritableException {
-        return userService.getAll();
     }
 
     @ApiOperation(value = "Generate JWT token and authenticate user",
