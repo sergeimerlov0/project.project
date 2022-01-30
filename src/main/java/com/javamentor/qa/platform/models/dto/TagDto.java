9bb311bc93e1a3805054eca7690b01ffe00ec5b1
Igor <predator.1199@mail.ru>
2022-01-30 23:28:51 +0300

    edit configuration

--- src/main/java/com/javamentor/qa/platform/models/dto/TagDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/TagDto.java
@@ -1,6 +1,5 @@
 package com.javamentor.qa.platform.models.dto;
 
-import com.javamentor.qa.platform.models.entity.question.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/UserDtoTestService.java
+++
@@ -1,6 +0,0 @@
-package com.javamentor.qa.platform.service.abstracts.dto;
-
-import com.javamentor.qa.platform.models.dto.UserTestDto;
-
-public interface UserDtoTestService extends PaginationServiceDto<UserTestDto>{
-}
