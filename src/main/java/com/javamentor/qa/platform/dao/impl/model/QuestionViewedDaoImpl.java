328d8bec2be3daa77bce72b317950c5eb0988d59
Сергей <skvortzovsergey@gmail.com>
2022-02-14 16:31:42 +0300

    исправил ключи кэширования

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/QuestionViewedDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/QuestionViewedDaoImpl.java
@@ -2,12 +2,8 @@ package com.javamentor.qa.platform.dao.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.QuestionViewedDao;
 import com.javamentor.qa.platform.models.entity.question.QuestionViewed;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -20,9 +16,8 @@ public class QuestionViewedDaoImpl extends ReadWriteDaoImpl<QuestionViewed, Long
     private EntityManager entityManager;
 
     @Override
-    @Cacheable(value = "checkMetod")
-    public boolean questionViewCheckByUserIdAndQuestionId (Long questionId, String email) {
-        String cacheKey = email + questionId;
+    @Cacheable(value = "checkMetod", key = "{#questionId,#email}")
+    public boolean questionViewCheckByUserIdAndQuestionId(Long questionId, String email) {
         return entityManager.createQuery("SELECT COUNT(a)>0 FROM QuestionViewed a WHERE a.question.id =:questionId and a.user.email =: useremail", Boolean.class)
                 .setParameter("questionId", questionId)
                 .setParameter("useremail", email)
@@ -30,8 +25,8 @@ public class QuestionViewedDaoImpl extends ReadWriteDaoImpl<QuestionViewed, Long
     }
 
     @Override
-    @CacheEvict(value = "checkMetod", allEntries = true)
-    public void persist (QuestionViewed questionViewed) {
+    @CacheEvict(value = "checkMetod", key = "{#questionViewed.question.id, #questionViewed.user.email}")
+    public void persist(QuestionViewed questionViewed) {
         super.persist(questionViewed);
     }
 
