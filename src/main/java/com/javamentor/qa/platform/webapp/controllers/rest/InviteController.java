c0327fe370b0634a730f358e1f8218fcff310479
Kaleev Alex <kaleevsasha@yandex.ru>
2022-02-26 17:14:11 +0300

    add variable in properties

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/InviteController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/InviteController.java
@@ -24,7 +24,7 @@ public class InviteController {
     private final UserService userService;
     private final RoleService roleService;
 
-    @Value("http://${jdbc.url:aDefaultUrl}/login")
+    @Value("${connection.url}/login")
     String loginEndPoint;
 
     @GetMapping()
