00a964817f5420bd85b1327c065d22bf61d47103
Skvortsov Sergey <sergeyskvortzov@gmail.com>
2022-03-06 18:01:09 +0300

    реализован  api question/sorted, который возвращает все вопросы, отсортированные по голосам, ответам и просмотрам

---
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/pagination/questionDto/AllQuestionsByVoitAndAnswerAndView.java
@@ -0,0 +1,60 @@
+package com.javamentor.qa.platform.dao.impl.dto.pagination.questionDto;
+
+import com.javamentor.qa.platform.dao.abstracts.dto.pagination.PaginationDtoAble;
+import com.javamentor.qa.platform.models.dto.QuestionViewDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Repository("AllQuestionDtoByVoitAndAnswerAndView")
+@RequiredArgsConstructor
+public class AllQuestionsByVoitAndAnswerAndView implements PaginationDtoAble<QuestionViewDto> {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<QuestionViewDto> getItems(Map<String, Object> param) {
+        int currentPageNumber = (int) param.get("currentPageNumber");
+        int itemsOnPage = (int) param.get("itemsOnPage");
+
+        return entityManager.createQuery(
+                        "SELECT DISTINCT new com.javamentor.qa.platform.models.dto.QuestionViewDto" +
+                                "(question.id, question.title, author.id, " +
+                                "(SELECT SUM (reputation.count) " +
+                                "FROM Reputation reputation " +
+                                "WHERE reputation.author.id = author.id), " +
+                                "author.fullName, author.imageLink, question.description, 0L, " +
+                                "(SELECT COUNT (*) " +
+                                "FROM Answer answer " +
+                                "WHERE answer.question.id = question.id) as countAnswer, " +
+                                "((SELECT COUNT (*) " +
+                                "FROM VoteQuestion voteOnQuestion " +
+                                "WHERE voteOnQuestion.vote = 'UP_VOTE' AND voteOnQuestion.question.id = question.id) - " +
+                                "(SELECT COUNT (*) " +
+                                "FROM VoteQuestion voteOnQuestion " +
+                                "WHERE voteOnQuestion.vote = 'DOWN_VOTE' AND voteOnQuestion.question.id = question.id)) as countVoite, " +
+                                "question.persistDateTime, question.lastUpdateDateTime)" +
+                                "FROM Question question " +
+                                "LEFT JOIN question.user AS author ON (question.user.id = author.id) " +
+                                "LEFT JOIN question.answers AS answer ON (question.id = answer.question.id) " +
+                                "WHERE question.isDeleted = false " +
+                                "ORDER BY countVoite, countAnswer"         //сюда надо будет добавить сортировку по ViewCount
+                        , QuestionViewDto.class)
+                .getResultStream()
+                .skip((long) itemsOnPage * (currentPageNumber - 1))
+                .limit(itemsOnPage)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public int getTotalResultCount(Map<String, Object> param) {
+        return Math.toIntExact((long) entityManager.createQuery(
+                        "SELECT count (*) FROM Question question WHERE question.isDeleted = false")
+                .getSingleResult());
+    }
+}
