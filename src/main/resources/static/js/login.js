c8191b16d2cf59e466c93ee2b6318654e6e6da3c
Eugene <evgenyisha85@gmail.com>
2022-03-10 22:51:19 +0900

    Cделал logout и очистку token из cookie

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -5,6 +5,27 @@
       new URL(document.referrer).hostname ===
       location.hostname);
 
+  function setCookie(name, value, options = {}) {
+    debugger
+    options = {
+      path: '/',
+      ...options
+    };
+    if (options.expires instanceof Date) {
+      options.expires = options.expires.toUTCString();
+    }
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+      updatedCookie += "; " + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += "=" + optionValue;
+      }
+    }
+    document.cookie = updatedCookie;
+  }
+
   form.addEventListener('submit', async function(event) {
     event.preventDefault();
 
@@ -25,7 +46,7 @@
     }
 
     const token = await response.text();
-    document.cookie = `token=${token}; expires=;`;
+    setCookie("token",token)
 
     location.replace(hasSameHostReferrer() ? document.referrer : '/main');
   });
