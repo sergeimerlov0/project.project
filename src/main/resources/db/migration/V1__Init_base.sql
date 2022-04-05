5aa975774033a84ee4d5f240e0a3d8d6233ce844
test123 <justnaruko26@gmail.com>
2022-04-05 19:50:44 +0500

    BookmarksTable fix

--- src/main/resources/db/migration/V1__Init_base.sql
+++ src/main/resources/db/migration/V1__Init_base.sql
@@ -15,7 +15,6 @@ create sequence tracked_tag_seq start 1 increment 1;
 create sequence user_seq start 1 increment 1;
 create sequence user_badges_seq start 1 increment 1;
 create sequence user_favorite_question_seq start 1 increment 1;
--- create sequence bookmarks_seq start 1 increment 1;
 create sequence vote_question_seq start 1 increment 1;
 
 CREATE TABLE answer
