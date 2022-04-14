035c06e9a313fff34c235b706a299f76cd29fc13
test123 <justnaruko26@gmail.com>
2022-04-14 18:28:01 +0500

    Api getBookmarksByUser

--- src/main/java/com/javamentor/qa/platform/models/dto/BookmarksDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/BookmarksDto.java
@@ -1,12 +1,10 @@
 package com.javamentor.qa.platform.models.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -14,20 +12,12 @@ import java.util.List;
 public class BookmarksDto {
     private Long id;
     private Long questionId;
-//    private String title;
-//    private Long userId;
-//    private List<TagDto> listTagDto;
-//    private Long countAnswer;
-//    private Long countVote;
-//    private Long countView;
     private LocalDateTime persistDate;
 
 
     public BookmarksDto(Long id, Long questionId, LocalDateTime persistDate) {
         this.id = id;
         this.questionId = questionId;
-//        this.title = title;
-//        this.countAnswer = countAnswer;
         this.persistDate = persistDate;
     }
 }
