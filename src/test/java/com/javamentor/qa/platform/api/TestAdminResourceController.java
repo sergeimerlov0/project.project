1a0a796ea92ead46c93de587939fbee9e73c33e9
Vitaly <kotovenko.vitaly@gmail.com>
2022-05-15 16:05:31 +0300

    Add method deleteAnswerByIt to AdminResourceController Tested

--- src/test/java/com/javamentor/qa/platform/api/TestAdminResourceController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestAdminResourceController.java
@@ -2,9 +2,13 @@ package com.javamentor.qa.platform.api;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.javamentor.qa.platform.AbstractApiTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class TestAdminResourceController extends AbstractApiTest {
@@ -33,5 +37,42 @@ public class TestAdminResourceController extends AbstractApiTest {
         this.mvc.perform(get("/api/user/101")
                         .header("Authorization", getJwtToken("2user@mail.ru", "123")))
                 .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    @DataSet(value = {
+            "datasets/AdminResourceController/deleteAnswerById/user.yml",
+            "datasets/AdminResourceController/deleteAnswerById/role.yml",
+            "datasets/AdminResourceController/deleteAnswerById/answer.yml",
+            "datasets/AdminResourceController/deleteAnswerById/question.yml",
+            "datasets/AdminResourceController/deleteAnswerById/questionHasTag.yml",
+            "datasets/AdminResourceController/deleteAnswerById/tag.yml",
+            "datasets/AdminResourceController/deleteAnswerById/voteAnswer.yml",
+            "datasets/AdminResourceController/deleteAnswerById/reputation.yml",
+
+    })
+    void deleteAnswerById() throws Exception {
+
+        //Проверка, что ответ существует и удален
+        this.mvc.perform(MockMvcRequestBuilders.delete("/api/admin/answer/100/delete")
+                        .header("Authorization", getJwtToken("test2@test.ru", "123")))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Answer is successfully deleted"));
+        Assertions.assertEquals(true, em.createQuery("SELECT a.isDeleted FROM Answer a WHERE a.id = 100").getSingleResult());
+        Assertions.assertEquals(true, em.createQuery("SELECT a.isDeletedByModerator FROM Answer a WHERE a.id = 100").getSingleResult());
+
+
+        //Проверка, что ответа не существует
+        this.mvc.perform(MockMvcRequestBuilders.delete("/api/admin/answer/103/delete")
+                        .header("Authorization", getJwtToken("test2@test.ru", "123")))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Answer is not exist"));
+
+        //Проверка, что ответа не пользователь не обладает правами
+        this.mvc.perform(MockMvcRequestBuilders.delete("/api/admin/answer/103/delete")
+                        .header("Authorization", getJwtToken("test3@test.ru", "123")))
+                .andExpect(status().isForbidden());
+
     }
 }
