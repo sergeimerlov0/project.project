b0b1c433b57d64d8a7d0107ad114ecc8faa12114
iana_909 <9098839557@mail.ru>
2022-06-06 15:43:24 +0200

    Разрешен конфликт: произведён импорт класса

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/AnswerDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/AnswerDtoDao.java
@@ -1,6 +1,8 @@
 package com.javamentor.qa.platform.dao.abstracts.dto;
 
 import com.javamentor.qa.platform.models.dto.AnswerDto;
+import com.javamentor.qa.platform.models.dto.AnswerUserDto;
+
 import java.util.List;
 import java.util.Optional;
 
