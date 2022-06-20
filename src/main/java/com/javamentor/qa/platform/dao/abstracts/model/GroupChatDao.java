a7132fb032ac6e8724da2b6aadc9783b086f98f5
Ilya Noskov <ilyanoskov1998@yandex.ru>
2022-06-20 16:37:18 +0300

    rework

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/GroupChatDao.java
+++
@@ -1,6 +0,0 @@
-package com.javamentor.qa.platform.dao.abstracts.model;
-
-import com.javamentor.qa.platform.models.entity.chat.GroupChat;
-
-public interface GroupChatDao extends ReadWriteDao<GroupChat, Long>{
-}
